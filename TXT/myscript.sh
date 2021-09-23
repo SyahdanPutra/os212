eda7324af1c212831872b449872f12b5d32f02c8
Syahdan Putra <70907059+SyahdanPutra@users.noreply.github.com>
2021-09-23 17:31:09 +0700

    W03 Assignment, mylog, myrank update

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
-# REV00 Sun 12 Sep 2021 03:10:00 WIB
-# START Sun 12 Sep 2021 03:10:00 WIB
+# Copyright (C) 2020-2021 Cicak Bin Kadal
 
-FILES="my*.txt my*.sh"
+# This free document is distributed in the hope that it will be 
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+# REV02 Sun 19 Sep 2021 15:44:11 WIB
+# REV01 Sun 12 Sep 2021 03:10:00 WIB
+# START Mon 28 Sep 21:05:04 WIB 2020
+
+# ATTN:
+# You new to set "REC2" with your own Public-Key Identity!
+# Check it out with "gpg --list-key"
+
+REC2="SPA"
+REC1="operatingsystems@vlsm.org"
+FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
+
+[ -d $HOME/RESULT/ ] || mkdir -p $HOME/RESULT/
+pushd $HOME/RESULT/
+for II in W?? ; do
+    [ -d $II ] || continue
+    TARFILE=my$II.tar.bz2
+    TARFASC=$TARFILE.asc
+    rm -f $TARFILE $TARFASC
+    echo "tar cfj $TARFILE $II/"
+    tar cfj $TARFILE $II/
+    echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
+    gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
+done
+popd
+
+for II in $HOME/RESULT/myW*.tar.bz2.asc ; do
+   echo "Check and move $II..."
+   [ -f $II ] && mv -f $II .
+done
 
 echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
